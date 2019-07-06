4a16444df4ea533ca7808fb4296c7905f0ca7b6f
g <guylilian@gmail.com>
2019-07-07 02:15:51 +0300

    vue material added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
 
