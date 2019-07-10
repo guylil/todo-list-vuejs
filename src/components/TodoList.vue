2d9dd289f4f39c354604092cd7d3d3a38e33a67e
g <guylilian@gmail.com>
2019-07-10 19:11:30 +0300

    first time data added and formatting

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
     <div class="filter-bar md-layout-nowrap">
-      <md-button class="md-layout-item md-xsmall-size-28 " @click="filterBy = 'all'"><md-icon>list</md-icon></md-button>
-      <md-button class="md-layout-item md-xsmall-size-28 " @click="filterBy = 'todo'">todo</md-button>
-      <md-button class="md-layout-item md-xsmall-size-28 " @click="filterBy = 'done'"><md-icon>done_all</md-icon></md-button>
-      <md-button class="md-layout-item md-xsmall-size-20 " @click="showNewTask=!showNewTask"><md-icon>add</md-icon></md-button>
+      <md-button
+        class="md-layout-item md-xsmall-size-28 "
+        @click="filterBy = 'all'"
+        ><md-icon>list</md-icon></md-button
+      >
+      <md-button
+        class="md-layout-item md-xsmall-size-28 "
+        @click="filterBy = 'todo'"
+        >todo</md-button
+      >
+      <md-button
+        class="md-layout-item md-xsmall-size-28 "
+        @click="filterBy = 'done'"
+        ><md-icon>done_all</md-icon></md-button
+      >
+      <md-button
+        class="md-layout-item md-xsmall-size-20 "
+        @click="showNewTask = !showNewTask"
+        ><md-icon>add</md-icon></md-button
+      >
     </div>
     <new-task v-if="showNewTask" @create-new-task="createNewTask"></new-task>
     <div v-for="task in tasksToShow" :key="task.taskId">
-      <task-item
-        :task-details="task"
-        @task-update="updateATask"
-      ></task-item>
+      <task-item :task-details="task" @task-update="updateATask"></task-item>
     </div>
   </div>
 </template>
@@ -60,11 +73,44 @@ export default {
       this.saveToLocalStorage();
     },
     getFromLocalStorage() {
-      if (localStorage.tasks) this.tasks = JSON.parse(localStorage.getItem("tasks"));
+      if (localStorage.tasks) {
+        this.tasks = JSON.parse(localStorage.getItem("tasks"));
+      } else {
+        this.addTasksForFirstTime();
+      }
     },
     saveToLocalStorage() {
-      localStorage.setItem("tasks",JSON.stringify(this.tasks));
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
     },
+    addTasksForFirstTime() {
+      this.tasks.push(
+        {
+          taskTitle: "Welcome to my to do list app",
+          taskDescription: "There are a lot of features here",
+          taskStatus: "todo",
+          taskId: 1
+        },
+        {
+          taskTitle: "Have a look",
+          taskDescription: "There are a lot of neat stuff",
+          taskStatus: "done",
+          taskId: 2
+        },
+        {
+          taskTitle: "Click around",
+          taskDescription:
+            "And you can see the code @ https://github.com/guylil/todo-list-vuejs --> on the Vuetify branch",
+          taskStatus: "done",
+          taskId: 3
+        },
+        {
+          taskTitle: "The app even works offline",
+          taskDescription: "Try it out",
+          taskStatus: "done",
+          taskId: 4
+        }
+      );
+    }
   },
   computed: {
     newTaskId() {
@@ -82,9 +128,9 @@ export default {
 </script>
 
 <style scoped>
-.filter-bar{
-    display: flex;
-    justify-content: space-around;
-    padding: 3px 0;
+.filter-bar {
+  display: flex;
+  justify-content: space-around;
+  padding: 3px 0;
 }
 </style>
